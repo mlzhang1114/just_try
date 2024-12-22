5fac0775ac52ced0292d8609cbf16f67039ecf0a
zeng <3194553664@qq.com>
2024-12-22 23:08:48 +0800

    new form分支测试

---
+++ new_form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace shujuku
+{
+    public partial class new_form : Form
+    {
+        public new_form()
+        {
+            InitializeComponent();
+        }
+    }
+}
